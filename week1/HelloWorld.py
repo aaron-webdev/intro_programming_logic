871668ebc20828568f21a423933a7a6e10a7276c
aaron-quickstart <aaron12clure@gmail.com>
2026-01-21 20:45:56 -0500

    week one notes added

--- week1/HelloWorld.py
+++ week1/HelloWorld.py
@@ -1,1 +1,21 @@
+
+# variable types:
+# integer - whole numbers
+# string - numbers and characters
+# float - decimals
+# boolean - true or false
+
+# no need to define variable type
+# "\n" inserts new line
+
+number = 12
+name = "Bob"
+number2 = 12.34
+on = True
+quote = "She asked \"How are you?\" " # the \ means literal
+
 print("Hello World")
+print(quote)
+
+
+
