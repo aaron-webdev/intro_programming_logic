259f6f6b24d49eb267c196b23b1c5bfc699816f7
aaron-quickstart <aaron12clure@gmail.com>
2026-01-28 20:03:41 -0500

    wk 2 notes

--- week2/myFisrtPythonCode.py
+++ week2/myFisrtPythonCode.py
@@ -1,1 +1,13 @@
-print("Hello World")
+# print("Hello World")
+# python uses the pound symbol to denote comments
+# indentaion is how Python starts a new line
+
+# firstName = "Mike"
+# firstName = firstName + "Sally"
+# quantity1 = 12
+# listPrice = 12.99
+
+quant1 = 1000
+quant2 = 22
+print( quant1 * quant2)
+print( quant1 // quant2)
