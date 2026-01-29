52e8957ba04926fbc5ebe92a664f2b08e8307669
aaron-quickstart <aaron12clure@gmail.com>
2026-01-28 20:33:40 -0500

    wk 2 notes

--- week2/myFisrtPythonCode.py
+++ week2/myFisrtPythonCode.py
@@ -7,7 +7,20 @@
 # quantity1 = 12
 # listPrice = 12.99
 
-quant1 = 1000
-quant2 = 22
-print( quant1 * quant2)
-print( quant1 // quant2)
+# quant1 = 1000
+# quant2 = 22
+# print( quant1 * quant2)
+# print( quant1 // quant2)
+
+# counter = 0
+# counter = counter + 1
+# print(counter)
+# counter += 1  # += is python's short hand for incrementing (++/ -- does not work in Python)
+# print(counter)
+
+# strings may have either double " or single ' quotes.  It is best to learn double quotes for consistency across other languages.
+# f-string (easier than concatination)
+# name = f"{lastName}, {firstName}"
+
+# when adding string and numbers together use "str()" to make the number a string data type
+
