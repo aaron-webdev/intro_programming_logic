16dbb547e4eed4f8ea66e3741b34523db2c98c0a
aaron-quickstart <aaron12clure@gmail.com>
2026-02-03 18:20:45 -0500

    minutes calculate inccorectly

--- week3/aclure_lab1.py
+++ week3/aclure_lab1.py
@@ -8,11 +8,13 @@ calcHours = 0
 calcMinutes = 0
 
 print("How far is your trip (in miles)?")
-userDistance = input()
+userDistance = int(input())
 print("How fast will you be driving (in miles per hour)?")
-userSpeed = input()
+userSpeed = int(input())
 
 calcHours = userDistance // userSpeed
 calcMinutes = userDistance % userSpeed
 
-print("Your trip will take {calcHours} hours and {calcMinutes} minutes.")
+print("Your trip will take")
+print("Hours: ", calcHours)
+print("Minutes: ",calcMinutes)
