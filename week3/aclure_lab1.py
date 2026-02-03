a7dd8f961c75d49712ab79b96619361314a51172
aaron-quickstart <aaron12clure@gmail.com>
2026-02-03 17:56:44 -0500

    init lab 1

---
+++ week3/aclure_lab1.py
@@ -0,0 +1,14 @@
+# Travel Time Calculator 
+# This program will take user inputs for distance and speed and calculate the travel time
+
+# intitialize variables and set to 0 to clear any unwated data
+userDistance = 0
+userSpeed = 0
+calcHours = 0
+calcMinutes = 0
+
+print("How far is your trip (in miles)?")
+userDistance = input()
+print("How fast will you be driving?")
+userSpeed = input()
+
