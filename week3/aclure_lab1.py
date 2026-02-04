ce35c90a3677495451b6da6f07b2d9c9a250a348
aaron-webdev <aaronclure.webdev@gmail.com>
2026-02-03 23:51:17 -0500

    refine code logic

--- week3/aclure_lab1.py
+++ week3/aclure_lab1.py
@@ -9,17 +9,17 @@ calcHours = 0
 calcMinutes = 0
 milesRemaining = 0
 
-print("How far is your trip (in miles)?")
-userDistance = int(input()) # force user input into a integer variable
-print("How fast will you be driving (in miles per hour)?")
-userSpeed = int(input()) # accept user input
-if userSpeed == 0 or userDistance == 0:  # force user input into a integer variable
+print("How far is your trip (tothe nearest miles)?")
+userDistance = int(input()) # force variable to float
+print("How fast will you be driving (to the nearest mile per hour)?")
+userSpeed = int(input()) # force variable to float
+if userSpeed == 0 or userDistance == 0:   # avoid infiinte computations
     print("Try again.  Enter only non-zero values.")
 
 calcHours = userDistance // userSpeed
 milesRemaining = userDistance % userSpeed # 
 calcMinutes = (milesRemaining * 60) // userSpeed
 
-print("Your trip will take")
+print("Your trip will take approximately")
 print("Hours: ", calcHours)
 print("Minutes: ",calcMinutes)
