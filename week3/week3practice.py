7700ce36500c1ff91bc402338d6724c62e011431
aaron-quickstart <aaron12clure@gmail.com>
2026-02-04 20:05:16 -0500

    wk 3 practice

--- week3/week3practice.py
+++ week3/week3practice.py
@@ -1,16 +1,28 @@
-# userName = "Aaron"
-# userAge = 29
+# print("What is your name?")
+# userName = input()
+# print("What is you age?")
+# userAge = int(input())
 # print(f"{userName} is {userAge} years old today!!")
 
-num1 = 7
-num2 = 9.5
 
-sum = num1+num2
-diff = num1-num2
-product = num1*num2
-quotient = round(num1/num2, 3)
+# print("\t Input first number.")
+# num1 = (float(input()))
+# print("\t Input second number.")
+# num2 = (float(input()))
 
-print(f"Sum: {sum}")
-print(f"Difference: {diff}")
-print(f"Product: {product}")
+# sum = num1+num2
+# diff = num1-num2
+# product = num1*num2
+# quotient = round(num1/num2, 3)
+
+# print(f"Sum: {sum}")
+# print(f"Difference: {diff}")
+# print(f"Product: {product}")
+# print(f"Quotient: {quotient}")
+
+print("\t Input first decimal.")
+num1 = (float(input()))
+print("\t Input second decimal.")
+num2 = (float(input()))
+quotient = round((num1/num2), 3)
 print(f"Quotient: {quotient}")
