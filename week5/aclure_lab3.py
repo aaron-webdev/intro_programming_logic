37f072a017f3d77207e47667295ea0560b3d5468
aaron-webdev <aaronclure.webdev@gmail.com>
2026-02-06 13:17:24 -0500

    Refined lab3 need restart function

--- week5/aclure_lab3.py
+++ week5/aclure_lab3.py
@@ -4,11 +4,33 @@
 
 
 # Accecpt and store user input
-print("\t Input the change value ex. x.xx")
-userChange=float(input())
+print("\t Input number of cents (0-99):")
+userChange=int(input())
+quart = 0
+dime = 0
+nick = 0
+penn = 0
 
-# Decrement by coin value while input value is greater than or equal to the coin value
-
+# Decrement by userChange while value is greater than or equal to the coin value
+while userChange > 24 :
+  quart += 1
+  userChange -= 25
+while userChange > 9 :
+  dime += 1
+  userChange -= 10
+while userChange > 4 :
+  nick += 1
+  userChange -= 5
+while userChange > 0 :
+  penn += 1
+  userChange -= 1
 # Store number of each coin needed to make input value equal zero
 
+
 # Output coin count
+print(f"\t Quarters: {quart}")
+print(f"\t Dimes:    {dime}")
+print(f"\t Nickles:  {nick}")
+print(f"\t Pennies:  {penn}")
+
+# Ask if user wants to restart the program
