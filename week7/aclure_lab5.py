3e195e04e91c9db476fe503c07432e36919be832
aaron-webdev <aaronclure.webdev@gmail.com>
2026-02-08 22:54:50 -0500

    built lab 5

--- week7/aclure_lab5.py
+++ week7/aclure_lab5.py
@@ -3,12 +3,12 @@
 # Create a program that will convert between feet and meters upon request
 
 def feetToMeters(userNum):
-    newMeters = userNum * 0.3048      # convert and return; 1ft = 0.3048m
+    newMeters = round(userNum * 0.3048,2)      # convert and return; 1ft = 0.3048m
     print(f"\t {newMeters} meters.")
     askToRepeat()
 
 def metersToFeet(userNum):
-    newFeet = userNum * 3.28084     # convert and return; 1m = 3.28084ft
+    newFeet = round(userNum * 3.28084,2)     # convert and return; 1m = 3.28084ft
     print(f"\t {newFeet} feet.")
     askToRepeat()
 
@@ -23,21 +23,23 @@ def askToRepeat():
 
 
 def main():
+    print()
     print("\t Feet and Meters Converter.")       # Ask the user for F >> M or F << M
     print()
     print("\tConversions Menu:")
     print("\tA: Feet to meters.")
     print("\tB: Meters to Feet.")
     userChoice = input()
+    print()
 
     if userChoice == "a" or userChoice == "A":
         print("\t Number of feet?")
-        userNum = int(input())      # store and pass value
+        userNum = float(input())      # store and pass value
         feetToMeters(userNum)
 
     elif userChoice == "b" or userChoice == "B":
         print("\t Number of meters?")
-        userNum = int(input())      # store and pass value
+        userNum = float(input())      # store and pass value
         metersToFeet(userNum)
 
     else:
