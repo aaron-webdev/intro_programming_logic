2f33837e769a7ae3439248351552e9e583a8fa98
aaron-quickstart <aaron12clure@gmail.com>
2026-02-09 18:15:14 -0500

    finished lab 6

--- week8/aclure_lab6.py
+++ week8/aclure_lab6.py
@@ -1,18 +1,12 @@
-#!/usr/bin/env python3
-
-TAX = 0.06
-
 def sales_tax(total): # added :
     sales_tax = total * 0.06 # changed to numeric value
-    newTotal = total * sales_tax
+    newTotal = total + sales_tax
     return newTotal
 
 def main():
     print("Sales Tax Calculator\n")
     total = float(input("Enter total: "))
-    sales_tax(total)
-    total_after_tax = round(newTotal, 2)
+    total_after_tax = round(sales_tax(total), 2)
     print("Total after tax: ", total_after_tax)
     
-if __name__ == "__main__":
-    main()
+main()
