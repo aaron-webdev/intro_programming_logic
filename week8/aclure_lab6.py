02b6b2a5366ae7ec13e2e1e34f3ad561246826d7
aaron-quickstart <aaron12clure@gmail.com>
2026-02-09 19:01:36 -0500

    finished lab 6

--- week8/aclure_lab6.py
+++ week8/aclure_lab6.py
@@ -1,12 +1,12 @@
 def sales_tax(total): # added :
     sales_tax = total * 0.06 # changed to numeric value
-    newTotal = total + sales_tax
-    return newTotal
+    newTotal = total + sales_tax # create new variable to store the new total
+    return newTotal # return the new calculated value
 
 def main():
     print("Sales Tax Calculator\n")
     total = float(input("Enter total: "))
-    total_after_tax = round(sales_tax(total), 2)
+    total_after_tax = round(sales_tax(total), 2) # call calculator function and pass user input, returns the new total
     print("Total after tax: ", total_after_tax)
     
 main()
