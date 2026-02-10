7a66b4dd2d0995467a1b5e2f2257b3ff76252886
aaron-quickstart <aaron12clure@gmail.com>
2026-02-10 00:07:46 -0500

    final final update....amybe.

---
+++ week9/aclure_lab7.py
@@ -0,0 +1,13 @@
+# Author : Aaron Clure
+# String and List Explorer
+# 1. Take a user input
+# 2. Demonstrate different ways to manipulate strings.
+
+# Count the words in the sentence.
+# Alphabetize [sort()]
+# Reverse alphabetize [reverse()]
+# Count user selected word [count()]
+# Print in all caps [upper()]
+# Print in all lower case [lower()]
+# Hyphenate the sentance [replace()]
+
