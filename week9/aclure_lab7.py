618269a940471520370427d6a024ca22d9719486
aaron-quickstart <aaron12clure@gmail.com>
2026-02-10 00:35:37 -0500

    update 1.0

--- week9/aclure_lab7.py
+++ week9/aclure_lab7.py
@@ -3,11 +3,19 @@
 # 1. Take a user input
 # 2. Demonstrate different ways to manipulate strings.
 
-# Count the words in the sentence.
-# Alphabetize [sort()]
-# Reverse alphabetize [reverse()]
-# Count user selected word [count()]
-# Print in all caps [upper()]
-# Print in all lower case [lower()]
-# Hyphenate the sentance [replace()]
+# Count the words in the sentence [count()].
+# Alphabetize [sort()].
+# Reverse alphabetize [reverse()].
+# Count user selected word [count()].
+# Print in all caps [upper()].
+# Print in all lower case [lower()].
+# Hyphenate the sentance [replace()].
+
+def main():
+    print(f"\t Enter a sentance:")
+    userPhrase = input()
+    print(F"\t Enter a word to count:")
+    trackWord = input()
+
+
 
