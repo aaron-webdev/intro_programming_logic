66bbd6ff60bddc1ba67ff3ad305eb504f5ae2dc0
aaron-quickstart <aaron12clure@gmail.com>
2026-02-18 18:50:26 -0500

    up to date.

---
+++ week5/inClassPractice.py
@@ -0,0 +1,15 @@
+# print("\t input two integers. Press enter between numbers.")
+# userNum1 = int(input())
+# userNum2 = int(input())
+# if userNum1 > userNum2 :
+#     print(f"\t {userNum1}")
+# elif userNum1 < userNum2 :
+#     print(f"\t{userNum2}")
+# else :
+#     print(f"\t{userNum1} = {userNum2}")
+
+count = 0
+while count < 10 :
+    if count%2 == 0:
+        print(count)
+    count = count + 1
