e4beb9a43973526ad4deb141cb71ab24b6f15e81
aaron-quickstart <aaron12clure@gmail.com>
2026-02-19 00:13:22 -0500

    started lab 7.

--- week9/aclure_lab7.py
+++ week9/aclure_lab7.py
@@ -3,8 +3,14 @@
 
 
 # Take a user input
+print("\t Input a sentence, then press enter.")
+userSent = input()
+print("\t Input a word from that sentence.")
+trackedWord = input()
+splitSent = userSent.split()
 # Demonstrate different ways to manipulate strings.
-# Count the words in the sentence [count()].
+# Sentence length
+wordCount = len(splitSent)
 # Alphabetize [sort()].
 # Reverse alphabetize [reverse()].
 # Count user selected word [count()].
@@ -14,10 +20,11 @@
 # Outputs
 
 
-# you need to make the string into a list before you can sort and do other list things
 
-# newList = userInput.split()
 
-# inside the () is for a delimiter.  
-# If left empty any white space will be the delimiter 
-# aka where the string is broken into substrings 
+# you need to make the string into a list, 
+# before you can sort and do other list things.
+# newList = userInput.split().
+# inside the () is for a delimiter.
+# If left empty any white space will be the delimiter.
+# aka where the string is broken into substrings.
