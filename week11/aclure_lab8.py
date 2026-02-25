c0e4d2ee620446d5e2e159fbc23cf417848891b8
aaron-webdev <aaronclure.webdev@gmail.com>
2026-02-24 20:46:14 -0500

    init lab 8

---
+++ week11/aclure_lab8.py
@@ -0,0 +1,11 @@
+# Aaron Clure
+# Wizard Inventory
+# Write code that keeps track and displays the invetory of a wizard
+# Introduction to Object-Oriented Programming
+
+# arrayName.append("New String added to the end of the array")
+# arrayName.pop(1) [removes the 2nd item in the given array(or other specified position)]
+# arrayName.remove("Name_Specific_Item_To_Remove")
+def main():
+    class Wizard() :
+        inventory = "Wooden Staff", "Wizard Hat", "Cloth Shoes"
