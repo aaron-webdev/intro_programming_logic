856de25f97be475cb138a6c98ff26150c8a901e5
aaron-webdev <aaronclure.webdev@gmail.com>
2026-02-26 16:16:56 -0500

    started lab 8 with lists

--- week11/aclure_lab8.py
+++ week11/aclure_lab8.py
@@ -7,3 +7,8 @@
 # arrayName.pop(1) [removes the 2nd item in the given array(or other specified position)]
 # arrayName.remove("Name_Specific_Item_To_Remove")
 
+#  USE A LIST for this lab
+# I think a list is similar to an array
+inventory = ["Wooden Staff","Wizard Hat","Cloth Shoes"]
+print(inventory)
+print(inventory[1])
