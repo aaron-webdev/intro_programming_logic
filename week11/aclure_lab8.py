924fb77721656f08d0d5be7154c64b32e0220a09
aaron-webdev <aaronclure.webdev@gmail.com>
2026-02-26 18:46:16 -0500

    add

--- week11/aclure_lab8.py
+++ week11/aclure_lab8.py
@@ -3,12 +3,9 @@
 # Write code that keeps track and displays the invetory of a wizard
 # Introduction to Object-Oriented Programming
 
-# arrayName.append("New String added to the end of the array")
-# arrayName.pop(1) [removes the 2nd item in the given array(or other specified position)]
-# arrayName.remove("Name_Specific_Item_To_Remove")
-
-#  USE A LIST for this lab
-# I think a list is similar to an array
-inventory = ["Wooden Staff","Wizard Hat","Cloth Shoes"]
-print(inventory)
-print(inventory[1])
+print(f"COMMAND MENU")
+print(f"show - Show all items")
+print(f"grab - Grab an item")
+print(f"edit - Edit an item")
+print(f"drop - Drop an item")
+print(f"exit - Exit program")
