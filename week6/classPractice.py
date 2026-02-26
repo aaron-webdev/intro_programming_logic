4791bba37fee0f4f007ed371044d3e536452f5dc
aaron-webdev <aaronclure.webdev@gmail.com>
2026-02-25 23:26:56 -0500

    updated class practices

--- week6/classPractice.py
+++ week6/classPractice.py
@@ -2,14 +2,19 @@
 
 
 
-def greet(userName):
+def greet(userName, greeting = "hello", punc = "!"):
     print()
-    print(f"Hello {userName}.")
+    print(f"{greeting} {userName}{punc}")
     
 def main():
     print(f"\tPlease tell me your name.")
     userName = input()
-    greet(userName)
+    print("Input a greeting")
+    greeting = input()
+    print("Input a punctuation")
+    punc = input()
+
+    greet(userName,greeting,punc)
 
 if __name__ == "__main__":
     main()
