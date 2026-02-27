2f61bff61e548623fad532c376edbb7a2e67ef60
aaron-webdev <aaronclure.webdev@gmail.com>
2026-02-26 19:28:41 -0500

    lab 8 progress. handles show request.

--- week11/aclure_lab8.py
+++ week11/aclure_lab8.py
@@ -1,11 +1,28 @@
 # Aaron Clure
 # Wizard Inventory
 # Write code that keeps track and displays the invetory of a wizard
-# Introduction to Object-Oriented Programming
 
-print(f"COMMAND MENU")
-print(f"show - Show all items")
-print(f"grab - Grab an item")
-print(f"edit - Edit an item")
-print(f"drop - Drop an item")
-print(f"exit - Exit program")
+invt = ["wooden staff","wizard hat","cloth shoes"]
+print(f"\n\tThe Wizard Inventory program\n")
+print(f"\tCOMMAND MENU:")
+print(f"\tshow - Show all items")
+print(f"\tgrab - Grab an item")
+print(f"\tedit - Edit an item")
+print(f"\tdrop - Drop an item")
+print(f"\texit - Exit program")
+
+print(f"Command:")
+userCmd = input()
+
+if userCmd == "show" :
+    i=1
+    for item in invt:
+        print(f"{i}.  {item}")
+        i+=1
+        
+elif userCmd == "grab":
+    if len(invt) == 4:
+        print(f"\tYou can't carry any more items. Drop something first.")
+    else:
+        print(f"Name the item: ")
+        invt = invt.append(input())
