4552f36b8d13012516465772519f1af37d758544
aaron-webdev <aaronclure.webdev@gmail.com>
2026-03-02 15:55:10 -0500

    worked on logic and cleaned up print formatting.

--- week11/aclure_lab8.py
+++ week11/aclure_lab8.py
@@ -12,6 +12,46 @@
 
 
 
+
+
+def main():
+    userCmd = input(f"\nCommand:")
+
+    if userCmd == "show" :  # loop through the list and print each item with its place number
+        i=1
+        for item in invt:
+            print(f"{i}.  {item}")
+            i+=1
+
+    elif userCmd == "grab": # limits carried items to 4
+        if len(invt) > 3:
+            print(f"You can't carry any more items. Drop something first.")
+        else:
+            invt.append(input(f"Name: "))
+            lastItem = invt[-1] # grabs last item in the list
+            print(f"{lastItem} was added.")
+
+    elif userCmd == "edit":
+        userNum = int(input(f"Number: "))
+        index = userNum - 1 # convert from place number to list index number
+        newName = input(f"Updated name: ")
+        invt[index] = newName
+        print(f"Item number {userNum} has been updated.")
+    
+    elif userCmd == "drop":
+        userNum = int(input(f"Number: "))
+        index = userNum - 1
+        droppedItem = invt.pop(index) # removes specific item from 
+        print(f"{droppedItem} was dropped.")
+
+    elif userCmd == "exit":
+        exit()
+
+    else:
+        print(f"Please input a valid command.")
+
+    main()
+
 invt = ["wooden staff","wizard hat","cloth shoes"]
 print(f"\nThe Wizard Inventory program\n")
 print(f"\nCOMMAND MENU:")
@@ -20,54 +60,6 @@ print(f"grab - Grab an item")       # add items to a list
 print(f"edit - Edit an item")       # acces an index within a list
 print(f"drop - Drop an item")       # remove items from a list
 print(f"exit - Exit program")
-
-def main():
-    print(f"\nCommand:")
-    userCmd = input()
-
-    if userCmd == "show" :
-        i=1
-        for item in invt:
-            print(f"{i}.  {item}")
-            i+=1
-
-    elif userCmd == "grab":
-        if len(invt) > 3:
-            print(f"You can't carry any more items. Drop something first.")
-        else:
-            print(f"Name: ")
-            invt.append(input())
-            lastItem = invt[-1]
-            print(f"{lastItem} was added.")
-
-    elif userCmd == "edit":
-        print(f"Number: ")
-        userNum = int(input())
-        index = userNum - 1
-        print(f"Updated name: ")
-        newName = input()
-        invt[index] = newName
-        print(f"Item number {userNum} has been updated.")
-    
-    elif userCmd == "drop":
-        print(f"Number: ")
-        userNum = int(input())
-        index = userNum - 1
-        droppedItem = invt.pop(index)
-        print(f"{droppedItem} was dropped.")
-
-    elif userCmd == "exit":
-        exit()
-
-    else:
-        print(f"Please input a valid item number.")
-
-
-    main()
-
-
-
-
 
 if __name__ == "__main__":
     main()
