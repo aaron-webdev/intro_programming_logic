3d71e39b19103b97161d6821a035b82a3920ac08
aaron-webdev <aaronclure.webdev@gmail.com>
2026-03-17 19:11:02 -0400

    add full file path to correctly open the txt file.

--- week12/aclure_lab9.py
+++ week12/aclure_lab9.py
@@ -1,5 +1,5 @@
 # Author: Aaron Clure
 # Read information from a file and display that information to the console.
 
-f = open("lab9_rules.txt")
+f = open("E:\PythonProgrammingLabs\intro_programming_logic\week12\lab9_rules.txt", "r")
 print(f.read())
