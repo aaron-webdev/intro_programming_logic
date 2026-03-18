3555402c63d5561bf9b75d16759c15262f1da50e
aaron-webdev <aaronclure.webdev@gmail.com>
2026-03-17 20:44:03 -0400

    init class labs.

---
+++ week13/aclure_lab10.py
@@ -0,0 +1,10 @@
+# Author: Aaron Clure
+# Email List Cleaner
+# Create a program that will read a file and create a formatted copy of the file.
+
+# open("file_name.file_type","x") : will create a file or return an error if the file exists
+# open("file_name.file_type","w") : will create a file if the file does not exist
+# open("file_name.file_type","a") : will create a file if the file does not exist
+# 
+# f = open("file_Name.fileType")
+# print(f.read())                   will read and then print file content to the terminal
