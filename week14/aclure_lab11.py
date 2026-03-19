245d64d0be2aa6cffd8de9961da63fdf94409c5f
aaron-webdev <aaronclure.webdev@gmail.com>
2026-03-19 19:58:48 -0400

    takes all inputs

--- week14/aclure_lab11.py
+++ week14/aclure_lab11.py
@@ -2,19 +2,44 @@
 # Lab 11 
 # Grade Calculator
 
-
+def getGradeAvg(labgrades,examgrades,quizgrades):
+    print(f"")
 
 def main():
     labGrades = []
+    examGrades = []
+    quizGrades = []
+
     print(f"Enter your lab grades. Enter a negative number to stop:")
     i=1
-    while (userGrade := float(input(f"Lab Grade {i}:"))) >= 0 :
+    while (userGrade := float(input(f"Lab Grade {i}: "))) >= 0 :         # gets all of user's lab grades
         labGrades.append(userGrade)
         i+=1
-    
-    for grade in labGrades:
-        print(f"grade: {grade}")
 
+    print(f"Enter your exam grades. Enter a negative number to stop:")
+    i=1
+    while (userGrade := float(input(f"Exam Grade {i}: "))) >= 0 :         # gets all of user's exam grades
+        examGrades.append(userGrade)
+        i+=1
+
+    print(f"Enter your quiz grades. Enter a negative number to stop:")
+    i=1
+    while (userGrade := float(input(f"Quiz Grade {i}: "))) >= 0 :         # gets all of user's quiz grades
+        quizGrades.append(userGrade)
+        i+=1
+    getGradeAvg(labGrades,examGrades,quizGrades)
+    
+    # print user input to check loading the arrays
+    #
+    # print(f"\n\n\n")
+    # for grade in labGrades:
+    #     print(f"lab grade: {grade}") 
+    
+    # for grade in examGrades:
+    #     print(f"exam grade: {grade}")
+    
+    # for grade in quizGrades:
+    #     print(f"quiz grade: {grade}")
 
 
 if __name__ == "__main__":
