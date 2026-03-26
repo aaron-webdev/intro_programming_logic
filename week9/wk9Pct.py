184b7d61343126e636644ef2c63df832eed796fa
aaron-webdev <aaronclure.webdev@gmail.com>
2026-03-25 21:15:08 -0400

    class notes.

---
+++ week9/wk9Pct.py
@@ -0,0 +1,58 @@
+#
+# !/usr/bin/env python3
+# 
+#  Define a list that has five integers in it
+# myIntegers = [1,2,3,4,5]
+# print(myIntegers)
+# oldValue = myIntegers[2]
+# myIntegers[2] = "BOB"
+# print(myIntegers)
+# print(oldValue)
+
+# append the number 3.14 to the list
+# inventory = ["staff", "hat", "robe", "bread", "bread", 42,42,234]
+# inventory.append(3.14)
+# print(inventory)
+
+# insert "bob" at index[3] and "sally" at index[5]
+# inventory = ["staff", "hat", "robe", "bread", "bread", 42,42,234]
+# inventory.insert(3, "bob")
+# inventory.insert(5, "sally")
+# print(inventory)
+
+# remove "bread" and 42
+# inventory = ["staff", "hat", "robe", "bread", "bread", 42,42,234]
+# inventory.remove("bread")
+# inventory.remove(42)
+# print(inventory)
+
+# find the index of "hat" and pop it off
+# inventory = ["staff", "hat", "robe", "bread", "bread", 42,42,234]
+# inventory.pop(inventory.index("hat"))
+# print(inventory)
+
+# inventory = ["staff", "hat", "robe", "bread","milk", "bread", 42,42,234]
+# for item in inventory:
+#     if item =="bread":
+#         inventory.remove(item)
+# print(inventory)
+
+# inventory = ["staff", "hat", "robe", "bread", "bread", 42,42,234]
+# item = "bread"
+# if item in inventory:
+#     inventory.remove(item)
+# print(inventory)
+
+# inventory = ["staff", "hat", "robe", "bread", "bread", 42,42,234]
+# for item in inventory:
+#     item = "bread"
+#     if item in inventory:
+#         inventory.remove(item)
+# print(inventory)
+
+# inventory = ["staff", "hat", "robe", "bread", "bread", 42,42,234]
+# for item in inventory:
+#     print(item)
+# print("\n\n")
+# for i in range(0,len(inventory)):
+#     print(inventory[i])
---
+++ week9/wk9classNotes.md
@@ -0,0 +1,18 @@
+# Debugging
+
+### Goal of testing and debugging
+- find all of errors befor deploying the program
+
+### types of errors
+- syntax errors
+    - compile time error
+    - caught before the program runs
+- runtime errors
+    - throws an exception that will stop the program
+- logic errors
+    - hardest to find
+    - everything works just not correctly
+    
+### steps of testing
+- use known valid data to test intended outcome
+- use known bad data to test for weakness
